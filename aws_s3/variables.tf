ba25b70d3d26fa63ad53a67c287b0c4aa80bf100
Arul-Abisheik <arulabisheik80127@gmail.com>
2022-05-10 11:43:20 +0530

    terraform_challenge

--- aws_s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-    type = string
-    default = "AKIA3K73SNGIYUAGTFHJ"
-}
-variable "aws_secret_key" {
-    type = string
-    default = "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"
- }
-
